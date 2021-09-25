b60553c1d3c3ef90d8b1722e22d48c999a87222e
Malan-Tai <malantai99@gmail.com>
2021-09-25 19:37:20 +0200

    Helper functions in grid map, and random tile generation

--- Assets/Scripts/GridMap.cs
+++ Assets/Scripts/GridMap.cs
@@ -5,18 +5,196 @@ using UnityEngine;
 public class GridMap : MonoBehaviour
 {
     [SerializeField]
-    private float _gridSize;
+    private float _cellSize;
+    public float CellSize { get { return _cellSize; } }
 
-    public Vector3 GridToWorld(Vector2 grid, float y)
+    [SerializeField]
+    private GameObject[] _tilePrefabs;
+
+    private Tile[,] _map;
+    // max x coordinate (exclusive)
+    private int _maxX;
+    // max y coordinate (exclusive)
+    private int _maxY;
+
+    public Tile this[Vector2Int u]
     {
-        return new Vector3(grid.x * _gridSize + _gridSize / 2, y, grid.y * _gridSize + _gridSize / 2);
+        get
+        {
+            if (!CellIsValid(u))
+            {
+                return null;
+            }
+            return _map[u.x, u.y];
+        }
     }
 
-    public Vector2 WorldToGrid(Vector3 world)
+    public Tile this[int x, int y]
     {
-        Vector2 gridPos = new Vector2();
-        gridPos.x = Mathf.Floor(world.x / _gridSize - 0.5f);
-        gridPos.y = Mathf.Floor(world.z / _gridSize - 0.5f);
+        get
+        {
+            if (!CellIsValid(x, y))
+            {
+                return null;
+            }
+            return _map[x, y];
+        }
+    }
+
+    private void Awake()
+    {
+        _maxX = 20;
+        _maxY = 20;
+        _map = new Tile[_maxX, _maxY];
+
+        for (int x = 0; x < _maxX; x++)
+        {
+            for (int y = 0; y < _maxY; y++)
+            {
+                int cover = Random.Range(0, 3);
+                _map[x, y] = new Tile(x, y, (Cover)cover);
+
+                if (cover < _tilePrefabs.Length)
+                {
+                    GameObject obj = Instantiate(_tilePrefabs[cover], this.transform);
+                    obj.transform.position = GridToWorld(new Vector2Int(x, y), 0);
+                    obj.transform.localScale = new Vector3(_cellSize, obj.transform.localScale.y, _cellSize);
+                }
+            }
+        }
+    }
+
+    public Vector3 GridToWorld(Vector2Int grid, float y)
+    {
+        return new Vector3(grid.x * _cellSize + _cellSize / 2, y, grid.y * _cellSize + _cellSize / 2);
+    }
+
+    public Vector2Int WorldToGrid(Vector3 world)
+    {
+        Vector2Int gridPos = new Vector2Int();
+        gridPos.x = (int)Mathf.Floor(world.x / _cellSize - 0.5f);
+        gridPos.y = (int)Mathf.Floor(world.z / _cellSize - 0.5f);
         return gridPos;
+    }
+
+    public Tile[] Neighbors(Vector2Int centerCell)
+    {
+        Tile[] neighbors = new Tile[8];
+        int i = 0;
+
+        int x = centerCell.x;
+        int y = centerCell.y;
+
+        bool canUp = (centerCell.y > 0);
+        bool canDown = (centerCell.y < _maxY - 1);
+        bool canLeft = (centerCell.x > 0);
+        bool canRight = (centerCell.x < _maxX - 1);
+
+        if (canUp)
+        {
+            Vector2Int nextCell = new Vector2Int(x, y - 1);
+            if (CanMoveFromCellToCell(centerCell, nextCell))
+            {
+                neighbors[i] = this[nextCell];
+                i++;
+            }
+        }
+        if (canDown)
+        {
+            Vector2Int nextCell = new Vector2Int(x, y + 1);
+            if (CanMoveFromCellToCell(centerCell, nextCell))
+            {
+                neighbors[i] = this[nextCell];
+                i++;
+            }
+        }
+        if (canLeft)
+        {
+            Vector2Int nextCell = new Vector2Int(x - 1, y);
+            if (CanMoveFromCellToCell(centerCell, nextCell))
+            {
+                neighbors[i] = this[nextCell];
+                i++;
+            }
+        }
+        if (canRight)
+        {
+            Vector2Int nextCell = new Vector2Int(x + 1, y);
+            if (CanMoveFromCellToCell(centerCell, nextCell))
+            {
+                neighbors[i] = this[nextCell];
+                i++;
+            }
+        }
+        if (canUp && canLeft)
+        {
+            Vector2Int nextCell = new Vector2Int(x - 1, y - 1);
+            if (CanMoveFromCellToCell(centerCell, nextCell))
+            {
+                neighbors[i] = this[nextCell];
+                i++;
+            }
+        }
+        if (canUp && canRight)
+        {
+            Vector2Int nextCell = new Vector2Int(x + 1, y - 1);
+            if (CanMoveFromCellToCell(centerCell, nextCell))
+            {
+                neighbors[i] = this[nextCell];
+                i++;
+            }
+        }
+        if (canDown && canLeft)
+        {
+            Vector2Int nextCell = new Vector2Int(x - 1, y + 1);
+            if (CanMoveFromCellToCell(centerCell, nextCell))
+            {
+                neighbors[i] = this[nextCell];
+                i++;
+            }
+        }
+        if (canDown && canRight)
+        {
+            Vector2Int nextCell = new Vector2Int(x + 1, y + 1);
+            if (CanMoveFromCellToCell(centerCell, nextCell))
+            {
+                neighbors[i] = this[nextCell];
+                i++;
+            }
+        }
+
+        Tile[] finalNeigh = new Tile[i];
+        int j;
+        for (j = 0; j < i; j++)
+        {
+            finalNeigh[j] = neighbors[j];
+        }
+
+        return finalNeigh;
+    }
+
+    // must add detection of units
+    public bool CanMoveFromCellToCell(Vector2Int cellA, Vector2Int cellB)
+    {
+        if (!CellIsValid(cellA) || !CellIsValid(cellB))
+        {
+            return false;
+        }
+
+        Tile tileA = _map[cellA.x, cellA.y];
+        Tile tileB = _map[cellB.x, cellB.y];
+
+        return (tileA.Cover == Cover.None && (tileB.Cover == Cover.Half || tileB.Cover == Cover.None)) ||
+            (tileA.Cover == Cover.Half && tileB.Cover == Cover.None);
+    }
+
+    public bool CellIsValid(Vector2Int u)
+    {
+        return u.x >= 0 && u.x < _maxX && u.y >= 0 && u.y < _maxY;
+    }
+
+    public bool CellIsValid(int x, int y)
+    {
+        return x >= 0 && x < _maxX && y >= 0 && y < _maxY;
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Priority_Queue;
+
+public enum Cover
+{
+    None,
+    Half,
+    Full
+}
+
+public class Tile : FastPriorityQueueNode
+{
+    private Cover _cover;
+    public Cover Cover { get { return _cover; } }
+
+    private Vector2Int _coords;
+    public Vector2Int Coords { get { return _coords; } }
+
+    public Tile(int x, int y, Cover cover)
+    {
+        _coords = new Vector2Int(x, y);
+        _cover = cover;
+    }
+
+    public float MoveCost(Vector2Int next)
+    {
+        Vector2Int difference = next - _coords;
+
+        if (difference.x == 0 || difference.y == 0) return 10f; // straight
+        return 14f; // diagonal ~= sqrt(2) * 10
+    }
+}
