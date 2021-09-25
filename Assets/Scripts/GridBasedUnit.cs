e548d9be8e06a0edff334b8474a51806f4dba94c
Malan-Tai <malantai99@gmail.com>
2021-09-25 19:34:12 +0200

    Grid based units now smoothly moves

--- Assets/Scripts/GridBasedUnit.cs
+++ Assets/Scripts/GridBasedUnit.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class GridBasedUnit : MonoBehaviour
 {
-    private Vector2 _gridPosition;
+    private Vector2Int _gridPosition;
+    private Vector3 _targetWorldPosition;
+    [SerializeField]
+    private float _moveSpeed;
 
     private void Start()
     {
@@ -13,6 +16,7 @@ public class GridBasedUnit : MonoBehaviour
 
         _gridPosition = gridMap.WorldToGrid(this.transform.position);
         this.transform.position = gridMap.GridToWorld(_gridPosition, this.transform.position.y);
+        _targetWorldPosition = this.transform.position;
 
         print(_gridPosition);
         print(transform.position);
@@ -25,10 +29,21 @@ public class GridBasedUnit : MonoBehaviour
         _timer += Time.deltaTime;
         if (_timer > 1f)
         {
-            _gridPosition += new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
+            MoveToNeighbor(new Vector2Int(Random.Range(-1, 2), Random.Range(-1, 2)));
             _timer = 0f;
-
-            this.transform.position = GameManager.Instance.gridMap.GridToWorld(_gridPosition, this.transform.position.y);
         }
+
+        Vector3 difference = _targetWorldPosition - this.transform.position;
+        if (difference.sqrMagnitude > GameManager.Instance.gridMap.CellSize / 100f)
+        {
+            Vector3 movement = difference.normalized * _moveSpeed * Time.deltaTime;
+            this.transform.position += movement;
+        }
+    }
+
+    public void MoveToNeighbor(Vector2Int deltaGrid)
+    {
+        _gridPosition += deltaGrid;
+        _targetWorldPosition = GameManager.Instance.gridMap.GridToWorld(_gridPosition, this.transform.position.y);
     }
 }
