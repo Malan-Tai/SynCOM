1ee61fff0f6500490fc6ffd8ce6222fdcab7f6ab
Guillaume BRANCHE <guillaume.branche@gmail.com>
2021-09-27 17:59:14 +0200

    added character classes skeletons and enums for races and classes

---
+++ Assets/Scripts/EnumTraits.cs
@@ -0,0 +1,27 @@
+public enum EnumTraits
+{
+    //FILLED WITH EXAMPLES FOR THE MOMENT
+
+    //common
+    Lovely,
+    Ugly,
+    Sprinter,
+
+    //dwarf
+    Stocky,
+    // berseker dwarf
+    BloodThirsted,
+    //Engineer dwarf
+    Smart,
+    
+    //elf
+    Racist,
+    //sniper
+    Accurate,
+    //hitman
+    StrongFist
+
+    //orc
+
+
+}
