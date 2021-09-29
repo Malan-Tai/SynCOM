eb1bd24f1982ba224bbc2aed58765e0ccc0b8d29
Guillaume BRANCHE <guillaume.branche@gmail.com>
2021-09-29 16:09:22 +0200

    add setters and getters

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -12,20 +12,78 @@ public class Character : MonoBehaviour
     //Character's archetype
 
     [SerializeField] private EnumClasses _class;
-    
+
     //Character's statistics
 
+    [SerializeField] private float _maxHealth;
     [SerializeField] private float _healthPoints;
-    [SerializeField] private float _damages;
-    [SerializeField] private float _accuracy;
-    [SerializeField] private float _dodge;
-    [SerializeField] private int _movementPoints;
-    [SerializeField] private int _weigth;
-
+    [SerializeField] private float _damage;    //amount of damages dealt
+    [SerializeField] private float _accuracy;  //value between 0 and 1
+    [SerializeField] private float _dodge;     // value between 0 and 1. Probability of successful attack is accuracy-dodge
+    [SerializeField] private float _movementPoints; // how far can a charcater move in one turn
+    [SerializeField] private float _weigth; //can be a condition for some actions
     
-    void Start()
+
+
+    //Getters and setters
+    public float MaxHealth
+    {
+        get { return this._maxHealth; }
+        private set { this._maxHealth = value; }
+    }
+    public float HealthPoints
+    {
+        get { return this._healthPoints; }
+        private set { this._healthPoints= value; }
+    }
+
+    public float Damages
+    {
+        get { return this._damage; }
+        private set { this._damage = value; }
+    }
+
+    public float Accuracy
+    {
+        get { return this._accuracy; }
+        private set { this._accuracy = value; }
+    }
+
+    public float Dodge
+    {
+        get { return this._dodge; }
+        private set { this._dodge = value; }
+    }
+
+    public float MovementPoints
+    {
+        get { return this._movementPoints ; }
+        private set { this._movementPoints = value; }
+    }
+
+    public float Weigth
+    {
+        get { return this._weigth; }
+        private set { this._weigth = value; }
+    }
+
+    void Start()    
     {
         
+    }
+
+    public void TakeDamages(float damages)
+    {
+        _healthPoints -= damages;
+        if (_healthPoints <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+
     }
 
     // Update is called once per frame
