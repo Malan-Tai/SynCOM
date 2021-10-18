b266e7b69064fdd0b695d116b77182fbef16f3c7
Malan-Tai <malantai99@gmail.com>
2021-10-18 14:57:25 +0200

    add ability to update and print emotions

--- Assets/Scripts/Debug/RelationshipDebugGUI.cs
+++ Assets/Scripts/Debug/RelationshipDebugGUI.cs
@@ -11,11 +11,23 @@ public class RelationshipDebugGUI : MonoBehaviour
         int i = 0;
         foreach (Relationship relationship in current.AllyCharacter.Relationships.Values)
         {
-            GUI.Box  (new Rect(10, i * 100 + 10, 100, 90), "Relationship #" + i);
+            GUI.Box(new Rect(10, i * 100 + 10, 100, 90), "Relationship #" + i);
 
-            GUI.Label(new Rect(20, i * 100 + 30, 80, 20), new GUIContent("Tru: " + relationship.GetGaugeValue(EnumSentiment.Trust)));
-            GUI.Label(new Rect(20, i * 100 + 50, 80, 20), new GUIContent("Adm: " + relationship.GetGaugeValue(EnumSentiment.Admiration)));
-            GUI.Label(new Rect(20, i * 100 + 70, 80, 20), new GUIContent("Sym: " + relationship.GetGaugeValue(EnumSentiment.Sympathy)));
+            GUI.Label(new Rect(20, i * 100 + 30, 80, 20), new GUIContent("TRU" + relationship.GetGaugeLevel(EnumSentiment.Trust) + ": " + relationship.GetGaugeValue(EnumSentiment.Trust)));
+            GUI.Label(new Rect(20, i * 100 + 50, 80, 20), new GUIContent("ADM" + relationship.GetGaugeLevel(EnumSentiment.Admiration) + ": " + relationship.GetGaugeValue(EnumSentiment.Admiration)));
+            GUI.Label(new Rect(20, i * 100 + 70, 80, 20), new GUIContent("SYM" + relationship.GetGaugeLevel(EnumSentiment.Sympathy) + ": " + relationship.GetGaugeValue(EnumSentiment.Sympathy)));
+
+            if (GUI.Button(new Rect(120, i * 100 + 10, 100, 20), "Update & Print"))
+            {
+                relationship.UpdateEmotions();
+
+                string emotionString = "emotions: ";
+                foreach (EnumEmotions emotion in relationship.ListEmotions)
+                {
+                    emotionString += emotion.ToString() + ", ";
+                }
+                print(emotionString);
+            }
 
             i++;
         }
