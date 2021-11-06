a6834ea07991b7870cca06dbae5f537eceb0376e
Guillaume BRANCHE <guillaume.branche@gmail.com>
2021-11-06 16:58:06 +0100

    range works and edd first traits

---
+++ Assets/Scripts/Characters/Traits/Naive.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Naive : Trait
+{
+    public override int GetSelfToAllySentimentGain(EnumSentiment sentiment, int baseSentiment)
+    {
+        if (sentiment == EnumSentiment.Trust && baseSentiment>0)
+        {
+            return (int) (baseSentiment * 1.5);
+        }
+        return baseSentiment;
+    }
+
+    public override int GetAllyToSelfSentimentGain(EnumSentiment sentiment, int baseSentiment)
+    {
+        if (sentiment == EnumSentiment.Trust && baseSentiment > 0)
+        {
+            return (int)(baseSentiment * 0.5);
+        }
+        return baseSentiment;
+    }
+    
+}
