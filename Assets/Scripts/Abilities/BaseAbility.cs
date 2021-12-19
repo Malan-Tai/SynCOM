6110d754e45619ea7938aa2c44495a9353159376
Malan-Tai <malantai99@gmail.com>
2021-12-19 18:11:19 +0100

    hover tile now works while using ability

--- Assets/Scripts/Abilities/BaseAbility.cs
+++ Assets/Scripts/Abilities/BaseAbility.cs
@@ -89,7 +89,6 @@ public abstract class BaseAbility
         bool confirmed = _uiConfirmed || Input.GetKeyDown(KeyCode.Return);
         bool cancelled = _uiCancelled || Input.GetKeyDown(KeyCode.Escape);
 
-
         if (confirmed && CanExecute())
         {
             Execute();
