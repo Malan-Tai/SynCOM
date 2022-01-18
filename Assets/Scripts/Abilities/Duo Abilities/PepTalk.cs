0007db8f55f5b383190c49ee99e5d629c9014050
RomainSntr <romain.santoro@telecom-sudparis.eu>
2022-01-18 17:23:24 +0100

    finished implementing SuppressiveFire, and started working on PepTalk

--- Assets/Scripts/Abilities/Duo Abilities/PepTalk.cs
+++ Assets/Scripts/Abilities/Duo Abilities/PepTalk.cs
@@ -11,9 +11,21 @@ public class PepTalk : BaseDuoAbility
 
     public override string GetDescription()
     {
-        return "Your words of encouragement strengthen you both, increasing damage, move and aim for the following turn.";
+        return "Your words of encouragement strengthen you both, increasing damage, move and aim for the following 2 turn." +
+               "\nDMG +20%" +
+               "\nACC +50%" +
+               "\nMOVE +3";
     }
 
+    public override string GetShortDescription()
+    {
+        return "A small stat buff to you and your ally.";
+    }
+
+    public override string GetAllyDescription()
+    {
+        return "You ally's words of encouragement strengthen you, increasing damage, move and aim for the following 2 turn.";
+    }
     public override string GetName()
     {
         return "Pep Talk";
@@ -21,37 +33,12 @@ public class PepTalk : BaseDuoAbility
 
     public override bool CanExecute()
     {
-        return _chosenAlly != null && _targetIndex >= 0;
+        return _chosenAlly != null;
     }
 
     protected override void ChooseAlly()
     {
-        _possibleTargets = new List<GridBasedUnit>();
-        var tempTargets = new GridBasedUnit[_effector.LinesOfSight.Count];
-        _effector.LinesOfSight.Keys.CopyTo(tempTargets, 0);
-
-        foreach (GridBasedUnit unit in tempTargets)
-        {
-            if (    (_chosenAlly.LinesOfSight.ContainsKey(unit)) 
-                &&  ((unit.GridPosition - this._effector.GridPosition).magnitude <= 3))
-            {
-                _possibleTargets.Add(unit);
-            }
-        }
-
-        RequestTargetsUpdate(_possibleTargets);
-
-        if (_possibleTargets.Count > 0)
-        {
-            _targetIndex = 0;
-            CombatGameManager.Instance.Camera.SwitchParenthood(_possibleTargets[_targetIndex]);
-            RequestTargetSymbolUpdate(_possibleTargets[_targetIndex]);
-        }
-        else RequestTargetSymbolUpdate(null);
-
-        _selfShotStats = new AbilityStats(200, 0, 1.5f, 0, _effector);
-
-        _selfShotStats.UpdateWithEmotionModifiers(_chosenAlly);
+       
     }
 
     protected override void EnemyTargetingInput()
@@ -62,19 +49,17 @@ public class PepTalk : BaseDuoAbility
     public override void Execute()
     {
         // Impact on the sentiments
+        AllyToSelfModifySentiment(_chosenAlly, EnumSentiment.Sympathy, 5);
+        SelfToAllyModifySentiment(_chosenAlly, EnumSentiment.Sympathy, 5);
+
         // Ally -> Self relationship
-        AllyToSelfModifySentiment(_chosenAlly, EnumSentiment.Trust, -10);
-        _effector.Character.Heal(6);
+        _effector.Character.CurrentBuffs.Add(new Buff(duration: 4, _effector, moveBuff: 3, damageBuff: 0.2f, accuracyBuff: 0.5f));
+        _chosenAlly.Character.CurrentBuffs.Add(new Buff(duration: 4, _chosenAlly, moveBuff: 3, damageBuff: 0.2f, accuracyBuff: 0.5f));
     }
 
     protected override bool IsAllyCompatible(AllyUnit unit)
     {
-        return (unit.GridPosition - this._effector.GridPosition).magnitude <= 1;
-    }
-
-    public override string GetAllyDescription()
-    {
-        return "The feasting spectacle is terrifying.";
+        return (unit.GridPosition - this._effector.GridPosition).magnitude <= 5;
     }
 
     public override void UISelectUnit(GridBasedUnit unit)
@@ -89,8 +74,4 @@ public class PepTalk : BaseDuoAbility
         else base.UISelectUnit(unit);
     }
 
-    public override string GetShortDescription()
-    {
-        return "A small stat buff to an ally.";
-    }
 }
