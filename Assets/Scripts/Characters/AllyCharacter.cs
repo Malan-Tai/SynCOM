966990f2ded4fb5b4d3bc794ae4074cf1e3e9ed2
RomainSntr <romain.santoro@telecom-sudparis.eu>
2022-01-21 12:13:26 +0100

    added LongShot and DwarfTossing to ability lists of Sniper and Berserker

--- Assets/Scripts/Characters/AllyCharacter.cs
+++ Assets/Scripts/Characters/AllyCharacter.cs
@@ -85,6 +85,7 @@ public class AllyCharacter : Character
                 instance._weigth            = 90;
 
                 instance.Abilities.Add(new Devouring());
+                instance.Abilities.Add(new DwarfTossing());
                 break;
             case EnumClasses.Engineer:
                 instance._maxHealth         = 25;
@@ -110,6 +111,7 @@ public class AllyCharacter : Character
                 instance._weigth            = 65;
 
                 instance.Abilities.Add(new SuppressiveFire());
+                instance.Abilities.Add(new LongShot());
                 break;
             case EnumClasses.Alchemist:
                 instance._maxHealth         = 15;
