6ce7b88705c79d19dd34b07ec78b70ec3e09a7c9
Malan-Tai <malantai99@gmail.com>
2022-01-27 12:19:52 +0100

    change some history colors

--- Assets/Scripts/UI/Combat/EntryColors.cs
+++ Assets/Scripts/UI/Combat/EntryColors.cs
@@ -5,6 +5,6 @@ public static class EntryColors
     public static readonly Color ICON_DUO_ABILITY = Color.cyan;
     public static readonly Color TEXT_ABILITY = Color.cyan;
     public static readonly Color TEXT_IMPORTANT = Color.yellow;
-    public static readonly Color LINK_UNIT = Color.red;
-    public static readonly Color LINK_UNIT_HOVER = Color.Lerp(Color.red, Color.black, 0.2f);
+    public static readonly Color LINK_UNIT = new Color(1f, 180f / 255f, 126f / 255f);
+    public static readonly Color LINK_UNIT_HOVER = Color.Lerp(LINK_UNIT, Color.black, 0.2f);
 }
